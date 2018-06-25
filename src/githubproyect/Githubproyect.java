2ad7495be0a02b6ea495b5ec26a1437a7bae357d
UsuarioN°1000 <UsuarioN1000@192.168.1.86>
2018-06-25 15:53:20 -0400

    cambios en mi master

--- src/githubproyect/Githubproyect.java
+++ src/githubproyect/Githubproyect.java
@@ -17,6 +17,7 @@ public class Githubproyect {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("yo trabajo en github!!");
+        System.out.println("yo trabajo en github desde la beta!!");
     }
     
 }

a04fc36ea865a68c33eb5949ccbf28c29548cad6
UsuarioN°1000 <UsuarioN1000@192.168.1.86>
2018-06-25 14:39:30 -0400

    commit oficial

--- src/githubproyect/Githubproyect.java
+++ src/githubproyect/Githubproyect.java
@@ -16,8 +16,7 @@ public class Githubproyect {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundoooo");
-        System.out.println("mundo!");
+        System.out.println("Lo único que quiero que este en esta rama es esto!");
         
     }
     
